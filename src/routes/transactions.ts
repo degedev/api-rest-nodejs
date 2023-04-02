c3e8c471d96c1865499e2579c8749315afc93eff
Douglas Ribeiro <dgrb0325@gmail.com>
2023-04-02 14:11:19 -0300

    create transactions get routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,9 +1,35 @@
 import { randomUUID } from 'crypto'
 import { FastifyInstance } from 'fastify'
-import knex from 'knex'
 import { z } from 'zod'
+import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return { summary }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
